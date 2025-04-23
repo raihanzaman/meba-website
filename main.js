22ff622133ae71e33c6fb8175e0c43a0aa8aa186
Raihan Zaman <raihanzaman.business@gmail.com>
2025-04-23 16:56:39 -0400

    Removed game tab

--- main.js
+++ main.js
@@ -11,50 +11,33 @@ mailing.addEventListener('submit', (event) => {
   emailInput.value = ''; // Clear the input field
 });
 
-// Navbar selector
+// Navbar pages selected
 const home_nav = document.querySelector('li#home');
 const eboard_nav = document.querySelector('li#eboard');
 const application_nav = document.querySelector('li#application');
-const game_nav = document.querySelector('li#game');
 
-// 
+// Div pages selected
 const home = document.querySelector('div#home');
 const eboard = document.querySelector('div#eboard');
 const application = document.querySelector('div#application');
-const game = document.querySelector('div#game');
 
 home_nav.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent the default form submission behavior
-
+  event.preventDefault();
   home.classList.remove('hidden')
   eboard.classList.add('hidden')
   application.classList.add('hidden')
-  game.classList.add('hidden')
 })
 
 eboard_nav.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent the default form submission behavior
-
+  event.preventDefault();
   eboard.classList.remove('hidden')
   home.classList.add('hidden')
   application.classList.add('hidden')
-  game.classList.add('hidden')
 })
 
 application_nav.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent the default form submission behavior
-
+  event.preventDefault();
   application.classList.remove('hidden')
   home.classList.add('hidden')
   eboard.classList.add('hidden')
-  game.classList.add('hidden')
-})
-
-game_nav.addEventListener('click', (event) => {
-  event.preventDefault(); // Prevent the default form submission behavior
-
-  game.classList.remove('hidden')
-  home.classList.add('hidden')
-  eboard.classList.add('hidden')
-  application.classList.add('hidden')
 })
