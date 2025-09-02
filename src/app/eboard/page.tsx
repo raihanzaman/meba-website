7571769629b10834821a65e95a1582317777baea
Raihan Zaman <raihanzaman.business@gmail.com>
2025-09-02 16:46:39 -0400

    Added to navbar and newsletter button works

--- src/app/eboard/page.tsx
+++ src/app/eboard/page.tsx
@@ -2,16 +2,10 @@
 
 export default function eboard() {
     return (
-        <div id="eboard">
-          <h1>MEBA Eboard</h1>
-          <p>This is the Eboard page for MEBA.</p>
-          <p>Here you can find information about the executive board members.</p>
-          <div className="members">
-            <h2>Executive Board Members</h2>
-            <div className="member">
-              <h3>Rami Awad</h3>
-            </div>
-          </div>
+      <div className="bg-[#D8B7DD] py-16">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+          <h2 className="text-4xl font-bold text-gray-900 mb-8">MEBA E-Board</h2>
         </div>
+      </div>
     )
 }
